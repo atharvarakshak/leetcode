60c869e7c9bf96762f4217443ecd4803f64c4f11
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-07-20 21:14:59 +0530

    Time: 4 ms (11.43%), Space: 20.7 MB (5.42%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -16,6 +16,32 @@ public:
 
     }
 
+    bool bfs(unordered_map<int,vector<int>> &adj, int cur, vector<int>& color, int curColor){
+        queue<int> q;
+
+        q.push(cur);
+        color[cur] = curColor;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int &v : adj[u]){
+                  if(color[v]==color[u]){
+                    return false;
+                }
+                if(color[v]==-1){
+                    color[v] = 1 - color[u];
+                    q.push(v);
+                    
+                }
+            }
+
+        }
+        return true;
+
+
+    }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         unordered_map<int,vector<int>> adj;
@@ -29,7 +55,8 @@ public:
 
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                if( dfs( adj, i, color, 1 )== false)return false;
+                // if( dfs( adj, i, color, 1 )== false)return false;
+                 if( bfs( adj, i, color, 1 )== false)return false;
             }
         }
 
