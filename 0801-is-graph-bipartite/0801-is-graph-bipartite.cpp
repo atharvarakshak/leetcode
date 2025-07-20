f28e572f9bd99626f86d4a1a215f6c308b6da5da
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-07-20 20:37:23 +0530

    Time: 4 ms (11.43%), Space: 20.4 MB (5.42%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    bool dfs(unordered_map<int,vector<int>> &adj, int cur, vector<int>& color, int curColor){
+        color[cur] = curColor;
+
+        for(auto &v : adj[cur]){
+            if(color[v]==curColor){
+                return false;
+            }
+            if(color[v]==-1){
+                if(dfs(adj,v,color, 1-curColor)==false)return false;
+            }
+        }
+        return true;
+
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        unordered_map<int,vector<int>> adj;
+       for(int i=0;i<n;i++){
+            for(auto it = graph[i].begin(); it!=graph[i].end(); it++){
+                adj[i].push_back(*it);
+            }
+       }
+
+        vector<int> color(n,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if( dfs( adj, i, color, 1 )== false)return false;
+            }
+        }
+
+        return true;
+    }
+};
