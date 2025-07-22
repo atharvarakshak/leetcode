bfa4486c1aaa06d18519efc5696d46592d937a93
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-07-22 08:59:39 +0530

    Time: 3 ms (58.35%), Space: 35.9 MB (68.41%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -8,21 +8,38 @@ public:
         if(x>arr[n-1]){
             return {arr.end()-k,arr.end()};
         }
-        vector<pair<int,int>> v;
+        // vector<pair<int,int>> v;
       
 
-        for(int i=0;i<n;i++){
-            v.push_back({abs(arr[i]-x),arr[i]});
-        }
+        // for(int i=0;i<n;i++){
+        //     v.push_back({abs(arr[i]-x),arr[i]});
+        // }
 
-        sort(v.begin(),v.end());
+        // sort(v.begin(),v.end());
         vector<int> res;
-        for(int i=0;i<k;i++){
-            res.push_back(v[i].second);
-        }
-            sort(res.begin(),res.end());
+        // for(int i=0;i<k;i++){
+        //     res.push_back(v[i].second);
+        // }
+        //     sort(res.begin(),res.end());
 
-        return res;
+        // return res;
+
+        int l=0,r=n-k;
+
+        while(l<r){
+            int m = l + (r-l)/2;
+
+            if(x-arr[m] > arr[m+k] -x ){
+                l = m+1;
+            }
+            else{
+                r=m;
+            }
+        }
+
+        return vector<int> (arr.begin()+l,arr.begin()+l+k);
+
+
 
     }
 };
