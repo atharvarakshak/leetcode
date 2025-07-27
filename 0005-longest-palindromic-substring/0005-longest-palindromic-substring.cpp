880eb966f3ea8e0e6905af91e3f55b8bfbb22c35
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-07-27 11:49:08 +0530

    Time: 121 ms (29.22%), Space: 17.6 MB (50.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -17,20 +17,52 @@ public:
     }
     string longestPalindrome(string s) {
        int n = s.length();
-        memset(dp,-1,sizeof(dp));
-        int c=0;
-        string str="";
+        // memset(dp,-1,sizeof(dp));
+        // int c=0;
+        // string str="";
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         if(isPalindrome(s,i,j)){
+        //            if((j-i+1)>c){
+        //             str = s.substr(i,(j-i+1));
+        //             c=(j-i+1);
+        //            }
+        //         }
+        //     }
+        // }
+
+        // bottom up
+        int t[1001][1001];
+        int maxL=0,idx=0;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)){
-                   if((j-i+1)>c){
-                    str = s.substr(i,(j-i+1));
-                    c=(j-i+1);
-                   }
+            t[i][i]=1;
+            maxL=1;
+        }
+        
+        for(int L=2;L<=n;L++){
+            for(int i=0;i<n-L+1;i++){
+                int j = (i+L-1);
+               
+                 if(L==2 && s[i]==s[j]){
+                    t[i][j]=1;
+                     maxL=2;
+                     idx=i;
+                }
+                else if(s[i]==s[j] && t[i+1][j-1]==1){
+                    t[i][j]=1;
+                    if((j-i+1)>maxL){
+                        maxL = (j-i+1);
+                        idx=i;
+                    }
+                }
+                else{
+                    t[i][j]=0;
                 }
             }
         }
 
-        return str;
+
+
+        return s.substr(idx,maxL);
     }
 };
