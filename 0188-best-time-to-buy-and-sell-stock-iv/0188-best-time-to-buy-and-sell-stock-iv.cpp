4ea4109de476b284aca3eeeeb13a58e6bff7c81d
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-08-10 22:13:51 +0530

    Time: 15 ms (31.82%), Space: 19.2 MB (15.41%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -24,6 +24,29 @@ int dp[1001][2][201];
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
            memset(dp,-1,sizeof(dp));
-        return solve(prices,n,k,1,0,0);
+        // return solve(prices,n,k,1,0,0);
+
+         vector<vector<int>> buy(k + 1, vector<int>(n));
+        //buy[t][i] = max profit after buying t'th stock on i'th index
+        vector<vector<int>> profit(k + 1, vector<int>(n));
+        //profit[t][i] = max profit after completing t tranx on i'th index
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int t = 1; t <= k; t++)
+            {
+                // max profit we can get on buying t'th stock here on i'th index
+                // = (-item price) + profit after completing t-1 tranx on i-1'th index
+                // or we have already bought t'th stock
+                if(i) buy[t][i] = max(buy[t][i - 1], profit[t - 1][i - 1] - prices[i]);
+                else buy[t][0] = -prices[i];
+
+                // max profit after completing total of t tranx
+                // = we completed t tranx on i-1'th index or we complete t tranx here
+                if(i) profit[t][i] = max(profit[t][i - 1], prices[i] + buy[t][i]);
+            }
+        }
+        
+        return profit[k][n - 1];
     }
 };
