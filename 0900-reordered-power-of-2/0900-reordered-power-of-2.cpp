34c100efc66fb23e16c0c7e9d24926c7be1951ca
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-08-10 15:31:05 +0530

    Time: 0 ms (100%), Space: 9.2 MB (41.89%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
+   string sortDigits(int num) {
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        return s;
+    }
     bool reorderedPowerOf2(int n) {
-        vector<long long> two;
-        for(int i=0;i<=29;i++){
-            two.push_back(pow(2,i));
-        }
-        string s = to_string(n);
-        unordered_map<char,int> freq;
+        string s = sortDigits(n);
 
-        for(int i=0;i<s.length();i++){
-            freq[s[i]]++;
-        }
-
-        for(int i=0;i<two.size();i++){
-            string str=to_string(two[i]);
-            unordered_map<char,int> mp;
-
-            for(int i=0;i<str.length();i++){
-                mp[str[i]]++;
+        for(int i = 0; i<31; i++){
+            int power = 1 << i;
+            if(s == sortDigits(power)){
+                return true;
             }
-            if(mp.size()!=freq.size())continue;
-            bool ok = true;
-            for(auto &f: freq){
-                if(freq[f.first]!=mp[f.first]){
-                    ok = false;
-                    break;
-                }
-            }
-            if(ok)return true;
-            mp.clear();
-
-
         }
         return false;
     }
+
+
+     
 };
