661d49eb11b163f9b798a489d4d45072db6a633b
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-08-17 14:12:15 +0530

    Time: 364 ms (36.74%), Space: 318.9 MB (43.26%) - LeetHub

--- 3887-minimum-cost-path-with-edge-reversals/3887-minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/3887-minimum-cost-path-with-edge-reversals.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges) {
+
+
         vector<vector<pair<int,int>>> adj(n);
-        for(auto& e : edges) {
-            adj[e[0]].push_back(make_pair(e[1], e[2]));
-            adj[e[1]].push_back(make_pair(e[0], 2 * e[2]));
+
+        for(auto e : edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            adj[e[1]].push_back({e[0],2*e[2]});
         }
 
         vector<int> dist(n,INT_MAX);
+
+        // pair<dist_so_far, node>
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
         pq.push({0,0});
-        while(pq.size()) {
-            auto [d,v] = pq.top();
+
+        while(!pq.empty()){
+            auto u = pq.top();
             pq.pop();
-            if(dist[v] != INT_MAX) continue;
-            dist[v] = d;
-            for(auto [x,w] : adj[v]) {
-                if(dist[x] == INT_MAX)
-                    pq.push({w + d,x});
+
+            int d = u.first, node = u.second ;
+
+            if(dist[node]!=INT_MAX)continue;
+            dist[node] = d;
+            for(auto &v : adj[node]){
+                if(dist[v.first]==INT_MAX){
+                    pq.push({v.second+d,v.first});
+                }
             }
         }
-        if(dist.back() == INT_MAX) {
-            return -1;
-        }
-        else{
-            return dist.back();
-        }
+
+        if(dist.back()==INT_MAX)return -1;
+
+        return dist.back();
     }
 };
