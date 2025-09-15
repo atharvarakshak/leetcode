cb23756c8b6cc7477b2e8246715e0527dad921c1
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-09-15 10:31:03 +0530

    Time: 8 ms (18.19%), Space: 19 MB (14.06%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
+int dp[1000][1000][2];
+    int solve(vector<int>& nums, int idx, int prev, int stat){
+        if(idx>=nums.size()){
+            return 0;
+        }
+        if(dp[idx][prev][stat]!=-1){
+            return dp[idx][prev][stat];
+        }
+        int take=0;
+        if(stat==0 && nums[idx]>nums[prev] || stat==1 && nums[idx]<nums[prev] ){
+            take = 1 + solve(nums,idx+1,idx, 1-stat);
+        }
+        int skip = solve(nums,idx+1,idx,stat);
 
-int solve(vector<int>& nums, int prev, int curr, bool isUp, vector<vector<vector<int>>>& dp) {
-    if(curr == nums.size()) return 0;
-
-    if(dp[prev+1][curr][isUp] != -1) return dp[prev+1][curr][isUp];
-
-    int take = 0;
-    // if no previous element or we can wiggle
-    if(prev == -1 || (isUp && nums[curr] > nums[prev]) || (!isUp && nums[curr] < nums[prev])) {
-        take = 1 + solve(nums, curr, curr+1, !isUp, dp);
+        return dp[idx][prev][stat] = max(take,skip);
+        
     }
-
-    int skip = solve(nums, prev, curr+1, isUp, dp);
-
-    return dp[prev+1][curr][isUp] = max(take, skip);
-}
 
     int wiggleMaxLength(vector<int>& nums) {
-         int n = nums.size();
-    // dp[prev+1][curr][isUp] = result
-    vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n, vector<int>(2, -1)));
-
-    return 1 + max(
-        solve(nums, 0, 1, true, dp),   // start expecting up
-        solve(nums, 0, 1, false, dp)   // start expecting down
-    );
+        memset(dp,-1,sizeof(dp));
+        return 1+max(solve(nums,1,0,0),solve(nums,1,0,1));
     }
 };
-
-
