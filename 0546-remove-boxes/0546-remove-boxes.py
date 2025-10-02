cb6cc7ba4006571dd484a7014f417ca48ad54cb0
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2025-10-02 14:35:31 +0530

    Time: 2479 ms (12.29%), Space: 68.3 MB (11.06%) - LeetHub

---
+++ 0546-remove-boxes/0546-remove-boxes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeBoxes(self, boxes: List[int]) -> int:
+        count=0
+        @lru_cache(None)
+        def dp(l,r,count=0):
+            if(l>r):
+                return count
+            if( l+1<=r and boxes[l]==boxes[l+1]):
+                l+=1
+                count+=1
+            
+            ans = (count+1)**2 + dp(l+1,r)
+
+            for m in range(l+1,r+1):
+                if(boxes[l]==boxes[m]):
+                    ans = max(ans,dp(m,r,count+1)+dp(l+1,m-1))
+
+            return ans
+
+
+        return dp(0,len(boxes)-1)
